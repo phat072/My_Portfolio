482cd2b8cad444cd85e990d7594a64f86855fe6d
Phan Cả Phát <phanphatlqd2003@gmail.com>
2023-12-06 01:46:49 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*========================Typing Animation========================*/
+var typed = new Typed(".typing", {
+  strings: ["", "Data Engineer", "Data Science", "System Engineer", ""],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
