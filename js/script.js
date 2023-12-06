9f0216a9a9f0126e8354ceb85dfd7dcbad70e4ba
Phan Cả Phát <phanphatlqd2003@gmail.com>
2023-12-06 16:48:27 +0700

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ var typed = new Typed(".typing", {
   BackSpeed: 60,
   loop: true,
 });
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropdownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function () {
+  dropdownMenu.classList.toggle("open");
+  const isOpen = dropdownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
